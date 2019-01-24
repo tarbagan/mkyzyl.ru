87f8fe04f2b40ccf0b103682c2d48ba35531ece7
Irgit <36793333+tarbagan@users.noreply.github.com>
2019-01-24 14:11:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ import wget
 from lxml.html.clean import Cleaner
 
 def link():
+    '''Генерируем ссылки для пагинации'''
     url = []
     for i in range(1,421):
         url.append("http://www.mkyzyl.ru/?PAGEN_1=%s" %i)
     return url
 
 def sanitize(dirty_html):
+    '''функция очистки мусора html'''
     cleaner = Cleaner(page_structure=True,
                   meta=True,
                   embedded=True,
@@ -32,18 +34,17 @@ def sanitize(dirty_html):
     return cleaner.clean_html(dirty_html)
 
 def parser_page(url):
+    '''Функция парсинга и сохранения данных'''
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
     response = requests.get(url, headers=headers ).text
     soup = bs(response, 'html.parser')
     tag = (soup.findAll('p', attrs={'class': 'news-item'}))
 
-    with open( 'd:/mykyzyl/news.txt', 'a', encoding='utf8') as file:
-
+    with open( 'news.txt', 'a', encoding='utf8') as file:
         for i in tag:
             url_full = "http://www.mkyzyl.ru" + (i.find("a").get('href'))
             response_page = requests.get(url_full, headers=headers).text
             soup_page = bs(response_page, 'html.parser')
-
             title = (soup_page.find("div", {"class": "welcome"})).text.strip()
             content_page = str((soup_page.findAll("div", {"class": "news"})[1:])[0].findAll("p")[1])
             content = (sanitize(content_page))
